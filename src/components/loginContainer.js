827d90936dc48a72aa9b9036759c9813e30bbc68
mcdunn51 <mattdunningweb@outlook.com>
2018-08-04 12:16:46 +0100

    basics of login component

---
+++ src/components/loginContainer.js
@@ -0,0 +1,78 @@
+import React from 'react';  
+import Radium from 'radium';
+
+const LogInContainer =  (props) => {
+
+    const style = {
+        formWrapper:{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            height: '100%',
+        },
+        form: {
+            width: '30%',
+            border: '10px solid green',
+            padding: '10px',
+            display: 'flex',
+            justifyContent: 'centre',
+            flexDirection: 'column',
+            borderRadius: '5px',
+        },
+        envelopeImg: {
+            maxWidth: '100%',
+            height: 'auto',
+            margin: 'auto',
+        },
+        formLabel: {
+            marginTop: '20px', 
+        },
+        input: {
+            marginTop: '10px',
+            border: '1px solid #ccc',
+            borderRadius: '4px',
+            padding: '12px 20px',
+        },
+        loginButton: {
+            marginTop: '20px',
+            border: '0px',
+            borderRadius: '4px',
+            padding: '14px 20px',
+        },
+        forgotPwdRememberMeContainer: {
+            marginTop: '20px',
+            marginBottom: '20px',
+        },
+        rememberMe: {
+            float: 'right',
+            clear: 'both',
+        }
+    };
+
+    return (
+        <div style={style.formWrapper}>
+            <form style={style.form} action="#">
+                
+                    <img style={style.envelopeImg} src={require("../images/email-icon.png")} alt="mail-img"/>
+
+                    <label style={style.formLabel} for="email"><b>Email</b></label>
+                    <input style={style.input} type="email" placeholder="Enter Email Address" name="email" required/>
+
+                    <label style={style.formLabel} for="psw"><b>Password</b></label>
+                    <input style={style.input} type="password" placeholder="Enter Password" name="password" required/>
+
+                    <button style={style.loginButton} type="submit">Login</button>
+
+                    <div style={style.forgotPwdRememberMeContainer}>
+                        <label style={style.rememberMe}>
+                            <input type="checkbox" checked="checked" name="remember" /> Remember me
+                        </label>
+                        <span class="psw">Forgot <a href="#">password?</a></span>
+                    </div>
+            </form>
+        </div>
+    );
+};
+
+export default Radium(LogInContainer);
